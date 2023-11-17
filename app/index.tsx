cdfa6fce3aaf1c6d0ee1936b36cec077bca7bff7
gracebir <grace.devolop@gmail.com>
2023-11-17 10:33:50 +0200

    use custom button

--- app/index.tsx
+++ app/index.tsx
@@ -2,11 +2,20 @@
 
 import { View, Text } from "react-native";
 import React from "react";
+import CustomButton from "../components/Button/CustomButton";
 
 const Page = () => {
     return (
-        <View>
+        <View
+            style={{
+                gap: 8,
+                padding: 10,
+            }}
+        >
             <Text>Page</Text>
+            <CustomButton text='Login' onPress={() => {}} />
+            <CustomButton text='Get Started' onPress={() => {}} />
+            <CustomButton text='Regiter' onPress={() => {}} />
         </View>
     );
 };
