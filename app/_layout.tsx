17e38f9a5661c458bcd0c04ef426913998c39369
gracebir <grace.devolop@gmail.com>
2023-11-17 09:35:41 +0200

    getstarted

---
+++ app/_layout.tsx
@@ -0,0 +1,47 @@
+/** @format */
+
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+
+export const unstable_settings = {
+    // Ensure that reloading on `/index` keeps a back button present.
+    initialRouteName: "index",
+};
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+    const [loaded, error] = useFonts({
+        PoppinsBold: require("../assets/fonts/Poppins-Bold.ttf"),
+        PoppinsSemiBold: require("../assets/fonts/Poppins-Bold.ttf"),
+        ...FontAwesome.font,
+    });
+
+    // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+    useEffect(() => {
+        if (error) throw error;
+    }, [error]);
+
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
+
+    if (!loaded) {
+        return null;
+    }
+
+    return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+    return (
+        <Stack>
+            <Stack.Screen name='index' options={{}} />
+        </Stack>
+    );
+}
---
+++ constants/Colors.ts
@@ -0,0 +1,23 @@
+const tintColorLight = '#2f95dc';
+const tintColorDark = '#fff';
+
+export default {
+  light: {
+    text: '#000',
+    background: '#fff',
+    tint: tintColorLight,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#fff',
+    background: '#000',
+    tint: tintColorDark,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorDark,
+  },
+  primary: "#62D2C3",
+  secondary: "#56C5B6",
+  baseColor: "#eeeeee",
+  grayish: "#7f7f7f"
+};
