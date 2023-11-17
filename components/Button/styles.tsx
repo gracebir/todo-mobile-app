4827220bd7183cce3c89d912f268c30ab1c36fd4
gracebir <grace.devolop@gmail.com>
2023-11-17 10:33:20 +0200

    button style

---
+++ components/Button/styles.tsx
@@ -0,0 +1,18 @@
+/** @format */
+
+import { StyleSheet } from "react-native";
+import Colors from "../../constants/Colors";
+
+export const styles = StyleSheet.create({
+    buttonContainer: {
+        backgroundColor: Colors.primary,
+        paddingVertical: 15,
+        borderRadius: 5,
+        alignItems: "center",
+    },
+    buttonText: {
+        color: "#000",
+        fontFamily: "Poppins_700Bold",
+        fontSize: 20,
+    },
+});
