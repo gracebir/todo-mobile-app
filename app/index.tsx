86f1940b2b285578279e6d269a29049a2439cd13
gracebir <grace.devolop@gmail.com>
2023-11-17 20:02:53 +0200

    update on custom text

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,50 @@
 /** @format */
 
-import { Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
-import TextLink from "../components/TextLink";
+import Colors from "../constants/Colors";
+import HeadingText from "../components/HeadingText";
+import CustomText from "../components/CustomText";
 
 const Page = () => {
     return (
-        <View>
-            <Text>Home page</Text>
-            <TextLink text='Go to login' href={"login"} />
-            <TextLink text='Go to register' href={"register"} />
-            <TextLink text='Go to dashboard' href={"dashboard"} />
+        <View style={styles.container}>
+            <View style={styles.imageContainer}>
+                <Image
+                    style={styles.image}
+                    source={require("../assets/data/1.png")}
+                />
+            </View>
+            <View style={styles.textContainer}>
+                <HeadingText text='Get things done with TODO' />
+                <CustomText
+                    isCentered={true}
+                    text={`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere gravida purus id eu condimentum est diam quam. Condimentum blandit diam.`}
+                />
+            </View>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: Colors.baseColor,
+        flex: 1,
+    },
+    imageContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 30,
+    },
+    image: {
+        width: 187.2,
+        height: 170,
+    },
+    textContainer: {
+        marginTop: 70,
+        alignItems: "center",
+        marginHorizontal: 45,
+    },
+});
 
 export default Page;
--- components/CustomText.tsx
+++ components/CustomText.tsx
@@ -6,15 +6,21 @@ import React, { FC } from "react";
 interface textProp {
     text: string;
     isLabel?: boolean;
+    isCentered?: boolean;
 }
 
-const CustomText: FC<textProp> = ({ text, isLabel = false }) => {
+const CustomText: FC<textProp> = ({
+    text,
+    isLabel = false,
+    isCentered = false,
+}) => {
     return (
         <Text
             style={{
                 fontFamily: "Poppins_600SemiBold",
                 fontSize: 15,
                 color: "#000",
+                textAlign: isCentered ? "center" : "auto",
                 marginLeft: isLabel ? 10 : 0,
             }}
         >
