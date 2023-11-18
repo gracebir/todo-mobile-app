3ed5b061efd73f1b4fc2391f47bf32c3e25f2b25
gracebir <grace.devolop@gmail.com>
2023-11-18 16:32:14 +0200

    dummy data

---
+++ assets/data/data.ts
@@ -0,0 +1,29 @@
+/** @format */
+
+export const taskItems = [
+    {
+        id: 1,
+        content: "Learn programming by 12pm",
+        isCompleted: false,
+    },
+    {
+        id: 2,
+        content: "Learn how to cook by 1pm",
+        isCompleted: false,
+    },
+    {
+        id: 3,
+        content: "Pick up the kids by 2pm",
+        isCompleted: false,
+    },
+    {
+        id: 4,
+        content: "Have lunch at 3pm",
+        isCompleted: false,
+    },
+    {
+        id: 5,
+        content: "Go visit  mum by 4pm",
+        isCompleted: false,
+    },
+];
