fa11702226e1bb6ee92dd0ce7d6b28ed8186551a
gracebir <grace.devolop@gmail.com>
2023-11-18 07:43:18 +0200

    start task list

---
+++ components/TaskList/TaskList.tsx
@@ -0,0 +1,15 @@
+/** @format */
+
+import { View, Text } from "react-native";
+import React from "react";
+import { styles } from "./style";
+
+const TaskList = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Daily Tasks</Text>
+        </View>
+    );
+};
+
+export default TaskList;
---
+++ components/TaskList/style.tsx
@@ -0,0 +1,14 @@
+/** @format */
+
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        height: 260,
+        width: "100%",
+        borderRadius: 10,
+        backgroundColor: "#FFF",
+        paddingHorizontal: 21,
+        paddingVertical: 12,
+    },
+});
