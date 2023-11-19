0ae08cfda9555a69ef16f268cce71395fb48a82d
gracebir <grace.devolop@gmail.com>
2023-11-19 20:26:57 +0200

    add onblur on the textinout

--- app/register.tsx
+++ app/register.tsx
@@ -1,100 +1,125 @@
 /** @format */
 
-import { View, ScrollView, StyleSheet } from "react-native";
-import React, { useState } from "react";
-import { Link } from "expo-router";
+import { View, ScrollView, StyleSheet, Text } from "react-native";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Toast from "react-native-toast-message";
+import { Formik } from "formik";
 import TextLink from "../components/TextLink";
 import CustomButton from "../components/Button/CustomButton";
 import CustomText from "../components/CustomText";
 import CustomTextInput from "../components/CustomTextInput/CustomTextInput";
 import HeadingText from "../components/HeadingText";
 import Colors from "../constants/Colors";
+import { signupSchema } from "../utils/baseSchema";
 
 const Register = () => {
-    const [form, setForm] = useState({
-        fullName: "",
-        email: "",
-        password: "",
-        confirmPassword: "",
-    });
     return (
-        <View style={styles.container}>
-            <ScrollView
-                showsVerticalScrollIndicator={false}
-                contentContainerStyle={{ flexGrow: 1 }}
-            >
-                <View style={styles.title}>
-                    <HeadingText text='Welcome Oboard!' />
-                    <CustomText text='Lets help you in completing your tasks' />
-                </View>
+        <Formik
+            initialValues={{
+                fullname: "",
+                email: "",
+                password: "",
+                confirmPassword: "",
+            }}
+            validationSchema={signupSchema}
+            onSubmit={(value) => console.log(value)}
+        >
+            {({
+                handleChange,
+                handleBlur,
+                values,
+                errors,
+                handleSubmit,
+                handleReset,
+            }) => (
+                <View style={styles.container}>
+                    <ScrollView
+                        showsVerticalScrollIndicator={false}
+                        contentContainerStyle={{ flexGrow: 1 }}
+                    >
+                        <View style={styles.title}>
+                            <HeadingText text='Welcome Oboard!' />
+                            <CustomText text='Lets help you in completing your tasks' />
+                        </View>
 
-                <View style={styles.formContainer}>
-                    <CustomTextInput
-                        label='Full Name'
-                        nativeID='fullname'
-                        keyBoardOption='default'
-                        value={form.fullName}
-                        placeholder='Mary Elliot'
-                        onChange={(fullName) => {
-                            setForm({
-                                ...form,
-                                fullName,
-                            });
-                        }}
-                    />
-                    <CustomTextInput
-                        label='Email'
-                        nativeID='email'
-                        keyBoardOption='email-address'
-                        value={form.email}
-                        placeholder='mary.elliot@mail.com'
-                        onChange={(email) => {
-                            setForm({
-                                ...form,
-                                email,
-                            });
-                        }}
-                    />
-                    <CustomTextInput
-                        label='Password'
-                        nativeID='password'
-                        keyBoardOption='default'
-                        isPassword={true}
-                        value={form.password}
-                        placeholder='**************'
-                        onChange={(password) => {
-                            setForm({
-                                ...form,
-                                password,
-                            });
-                        }}
-                    />
-                    <CustomTextInput
-                        label='Confirm'
-                        nativeID='confirm'
-                        keyBoardOption='default'
-                        isPassword={true}
-                        value={form.password}
-                        placeholder='**************'
-                        onChange={(confirmPassword) => {
-                            setForm({
-                                ...form,
-                                confirmPassword,
-                            });
-                        }}
-                    />
+                        <View style={styles.formContainer}>
+                            <CustomTextInput
+                                label='Full Name'
+                                onBlur={handleBlur("fullname")}
+                                nativeID='fullname'
+                                keyBoardOption='default'
+                                value={values.fullname}
+                                placeholder='Mary Elliot'
+                                onChange={handleChange("fullname")}
+                            />
+                            {errors.fullname && (
+                                <Text style={{ fontSize: 10, color: "red" }}>
+                                    {errors.fullname}
+                                </Text>
+                            )}
+                            <CustomTextInput
+                                label='Email'
+                                nativeID='email'
+                                keyBoardOption='email-address'
+                                value={values.email}
+                                placeholder='mary.elliot@mail.com'
+                                onBlur={handleBlur("email")}
+                                onChange={handleChange("email")}
+                            />
+                            {errors.email && (
+                                <Text style={{ fontSize: 10, color: "red" }}>
+                                    {errors.email}
+                                </Text>
+                            )}
+                            <CustomTextInput
+                                label='Password'
+                                nativeID='password'
+                                keyBoardOption='default'
+                                isPassword={true}
+                                value={values.password}
+                                placeholder='**************'
+                                onBlur={handleBlur("password")}
+                                onChange={handleChange("password")}
+                            />
+                            {errors.password && (
+                                <Text style={{ fontSize: 10, color: "red" }}>
+                                    {errors.password}
+                                </Text>
+                            )}
+                            <CustomTextInput
+                                label='Confirm'
+                                nativeID='confirm'
+                                keyBoardOption='default'
+                                isPassword={true}
+                                value={values.confirmPassword}
+                                placeholder='**************'
+                                onBlur={handleBlur("confirmPassword")}
+                                onChange={handleChange("confirmPassword")}
+                            />
+                            {errors.confirmPassword && (
+                                <Text style={{ fontSize: 10, color: "red" }}>
+                                    {errors.confirmPassword}
+                                </Text>
+                            )}
+                        </View>
+                        <View style={styles.buttonContainer}>
+                            <CustomButton
+                                text='Register'
+                                onPress={() => {
+                                    handleSubmit();
+                                }}
+                            />
+                            <View style={styles.linkText}>
+                                <CustomText text='Already have an account ?  ' />
+                                <TextLink text='Sign In?' href={"/login"} />
+                            </View>
+                        </View>
+                    </ScrollView>
+                    <Toast />
                 </View>
-                <View style={styles.buttonContainer}>
-                    <Link href={"/dashboard"} asChild>
-                        <CustomButton text='Register' />
-                    </Link>
-                    <View style={styles.linkText}>
-                        <CustomText text='Already have an account ?  ' />
-                        <TextLink text='Sign In?' href={"/login"} />
-                    </View>
-                </View>
-            </ScrollView>
-        </View>
+            )}
+        </Formik>
     );
 };
 
