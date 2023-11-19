2ccc13f26f48f4c97b5e38fdebed70c044999e69
gracebir <grace.devolop@gmail.com>
2023-11-19 19:32:58 +0200

    auth slice extra slice

--- slices/auth.slice.ts
+++ slices/auth.slice.ts
@@ -1,6 +1,7 @@
 /** @format */
 
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+import { loginUser, registerUser } from "../actions/auth.action";
 
 const initialState = {
     loading: false,
@@ -14,6 +15,38 @@ const authSlice = createSlice({
     name: "auth",
     initialState,
     reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(registerUser.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+                state.success = false;
+            })
+            .addCase(registerUser.fulfilled, (state) => {
+                state.loading = false;
+                state.success = true;
+            })
+            .addCase(registerUser.rejected, (state, action) => {
+                state.loading = false;
+                //@ts-ignore
+                state.error = action.payload;
+                state.success = false;
+            })
+            .addCase(loginUser.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(loginUser.fulfilled, (state, { payload }) => {
+                state.loading = false;
+                state.userInfo = payload;
+                state.userToken = payload.token;
+            })
+            .addCase(loginUser.rejected, (state, { payload }) => {
+                state.loading = false;
+                //@ts-ignore
+                state.error = payload;
+            });
+    },
 });
 
 export default authSlice.reducer;
