2ace842145ecf52626aa7bdf02d933f0cc21b910
gracebir <grace.devolop@gmail.com>
2023-11-20 11:37:29 +0200

    open the modal

--- components/TaskList/TaskList.tsx
+++ components/TaskList/TaskList.tsx
@@ -8,12 +8,18 @@ import {
     ListRenderItem,
     FlatList,
 } from "react-native";
-import React from "react";
+import React, { useRef } from "react";
 import { styles } from "./style";
 import TaskItem, { taskIteProps } from "../TaskItem";
 import { taskItems } from "../../assets/data/data";
+import { BottomSheetModal } from "@gorhom/bottom-sheet";
+import BottomSheet from "../ButtomSheet/ButtomSheet";
 
 const TaskList = () => {
+    const bottomSheetRef = useRef<BottomSheetModal>(null);
+    const openModal = () => {
+        bottomSheetRef.current?.present();
+    };
     const renderItem: ListRenderItem<taskIteProps> = ({ item }) => (
         <TaskItem
             key={item.id}
@@ -25,7 +31,7 @@ const TaskList = () => {
         <View style={styles.container}>
             <View style={styles.taskHeader}>
                 <Text style={styles.taskHeaderTitle}>Daily Tasks</Text>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={openModal}>
                     <Image
                         style={styles.plusImage}
                         source={require("../../assets/data/plusadd.png")}
@@ -41,6 +47,7 @@ const TaskList = () => {
                     renderItem={renderItem}
                 />
             </View>
+            <BottomSheet ref={bottomSheetRef} />
         </View>
     );
 };
