4acb4fe4e3f574e2b2f8ab4d3b690fe9be1eefc6
gracebir <grace.devolop@gmail.com>
2023-11-20 13:27:39 +0200

    task slice

--- slices/task.slice.ts
+++ slices/task.slice.ts
@@ -1,16 +1,55 @@
 /** @format */
 
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+// import { createSlice, } from "@reduxjs/toolkit";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const initialState = {
-    tasks: {},
-    isLoading: false,
-};
+interface Itask {
+    id: number;
+    content: string;
+    isCompleted: boolean;
+    userId: number;
+}
 
-const taskSlice = createSlice({
-    name: "tasks",
-    initialState,
-    reducers: {},
+const backendURL = "https://task-api-k86t.onrender.com";
+
+export const taskSliceApi = createApi({
+    reducerPath: "task",
+    baseQuery: fetchBaseQuery({
+        baseUrl: backendURL,
+    }),
+    tagTypes: ["Itask"],
+    endpoints: (builder) => ({
+        getAllTasks: builder.query<Itask[], number>({
+            query(userId: number) {
+                return {
+                    url: `/api/task/${userId}`,
+                    method: "GET",
+                };
+            },
+            providesTags: (result) =>
+                result
+                    ? [
+                          ...result.map(
+                              ({ id }) => ({ type: "Itask", id } as const)
+                          ),
+                          { type: "Itask", id: "LIST" },
+                      ]
+                    : [{ type: "Itask", id: "LIST" }],
+        }),
+        addtask: builder.mutation<
+            Itask,
+            Partial<Itask> & Pick<Itask, "userId">
+        >({
+            query({ userId, ...body }) {
+                return {
+                    url: `/api/task/create/${userId}`,
+                    method: "POST",
+                    body,
+                };
+            },
+            invalidatesTags: [{ type: "Itask", id: "Itask" }],
+        }),
+    }),
 });
 
-export default taskSlice.reducer;
+export const { useGetAllTasksQuery, useAddtaskMutation } = taskSliceApi;
