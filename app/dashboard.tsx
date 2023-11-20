8f754744eb124b42a50a2e8c0a07e770fb377c49
gracebir <grace.devolop@gmail.com>
2023-11-20 07:05:12 +0200

    get username from database

--- app/dashboard.tsx
+++ app/dashboard.tsx
@@ -1,15 +1,20 @@
 /** @format */
 
-import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
 import { useNavigation } from "expo-router";
 import Colors from "../constants/Colors";
-import CustomText from "../components/CustomText";
 import HeadingText from "../components/HeadingText";
 import TaskList from "../components/TaskList/TaskList";
+import { useSelector } from "react-redux";
+import { RootState } from "../utils/rootReducer";
 
 const DashBoard = () => {
     const navitagation = useNavigation();
+    const { userInfo } = useSelector((state: RootState) => state.auth);
+
+    //@ts-ignore
+    console.log(userInfo.user.fullname);
     return (
         <View style={styles.container}>
             <View style={styles.imageContainer}>
@@ -17,7 +22,9 @@ const DashBoard = () => {
                     style={styles.image}
                     source={require("../assets/images/emilie.png")}
                 />
-                <Text style={styles.title}>Welcome Mary</Text>
+                <Text style={styles.title}>
+                    Welcome {userInfo.user.fullname}
+                </Text>
             </View>
             <View style={styles.content}>
                 <View style={styles.clockImageContainer}>
